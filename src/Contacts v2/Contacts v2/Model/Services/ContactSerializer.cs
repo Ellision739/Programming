4eec50680f982e4185d55e847f62e578599d36c3
Ellision739 <danila.schamov@yandex.ru>
2026-03-24 23:00:18 +0700

    features: 3_simple_application_with_collection Create application in "Contacts v2" with collection of contacts. Can be saved and recorded in sessions

---
+++ src/Contacts v2/Contacts v2/Model/Services/ContactSerializer.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace View.Model.Services;
+
+/// <summary>
+/// Серелизирует контакты с типом json.
+/// </summary>
+public class ContactSerializer
+{
+    /// <summary>
+    /// Возвращает и задаёт путь к файлу.
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// Путь к папки документов.
+    /// </summary>
+    private string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+    /// <summary>
+    /// Загружает контакты из json формата.
+    /// </summary>
+    /// <returns>Список контактов типа <see cref="Contact"/>.</returns>
+    public ObservableCollection<Contact> Load()
+    {
+        if (!File.Exists(Path))
+        {
+            return new ObservableCollection<Contact>();
+        }
+
+        if (File.ReadAllText(Path) == "")
+        {
+            return new ObservableCollection<Contact>();
+        }
+
+        string text = File.ReadAllText(Path);
+        var contacts = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(text);
+        return contacts;
+    }
+    /// <summary>
+    /// Сохраняет контакты в файл json формата.
+    /// </summary>
+    /// <param name="contacts">Список контактов типа <see cref="Contact"/>.</param>
+    public void Save(ObservableCollection<Contact> contacts)
+    {
+        if (Directory.Exists(System.IO.Path.GetDirectoryName(Path)) == false)
+        {
+            Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Path));
+        }
+
+        File.WriteAllText(Path, JsonConvert.SerializeObject(contacts));
+    }
+
+    /// <summary>
+    /// Создаёт экземпляр класса <see cref="ContactSerializer"/>.
+    /// </summary>
+    public ContactSerializer()
+    {
+        Path = System.IO.Path.Combine(docPath, "Contacts v2/contacts.json");
+    }
+}
